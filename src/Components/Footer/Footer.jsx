07b3f22a6fecd8cab29ed39808c6311aa0708401
junlee91 <jlee757@uic.edu>
2019-03-31 17:46:04 -0500

    Header & Footer component

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+`;
+
+const FooterItem = styled.h6`
+  font-size: 16px;
+  font-weight: 300;
+  padding: 3px 15px 3px 15px;
+`;
+
+const Footer = () => (
+  <Container>
+    <Link to="/">
+      <FooterItem>Home</FooterItem>
+    </Link>
+    <h3>|</h3>
+    <Link to="/about">
+      <FooterItem>About CareerTalk</FooterItem>
+    </Link>
+    <h3>|</h3>
+    <Link to="/">
+      <FooterItem>Get Involved</FooterItem>
+    </Link>
+  </Container>
+);
+
+export default Footer;
