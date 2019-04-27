4df1aea3ede9348970aaa97585c8ac747812748e
junlee91 <jlee757@uic.edu>
2019-04-26 22:24:07 -0500

    Google Sign in with GraphQL

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery, useMutation } from 'react-apollo-hooks';
+import { gql, } from 'apollo-boost';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import styled from 'styled-components';
 
 import withRouteComponent from './withRouteComponent';
 import { Container } from '../Components/commons';
@@ -7,27 +10,100 @@ import AppConfig from '../config.json';
 
 const googleClientId = AppConfig.GOOGLE_CLIENT_ID;
 
-const responseGoogle = response => {
-  console.log(response.tokenId);
-  console.log(response.googleId);
-};
+const Title = styled.h1`
+  font-size: 20px;
+  font-weight: 600;
+  padding: 15px 0;
+`;
 
-const Home = props => (
-  <Container isSideBar={props.isSideBar}>
-    <h1>Home</h1>
-    <GoogleLogin
-      clientId={googleClientId}
-      buttonText="Login with Google"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      cookiePolicy="single_host_origin"
-    />
-    <GoogleLogout
-      clientId={googleClientId}
-      buttonText="Logout"
-      onLogoutSuccess={() => console.log('logged out!')}
-    />
-  </Container>
-);
+const HomeContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 15px;
+`;
+
+// ---------------------------------------------------------------------------- //
+const GOOGLE_SIGN_IN = gql`
+  mutation userLogin($tokenId: String!, $googleId: String!) {
+    userLogin(tokenId: $tokenId, googleId: $googleId)
+  }
+`;
+
+const LOCAL_LOG_IN = gql`
+  mutation logUserIn($token: String!) {
+    logUserIn(token: $token) @client
+  }
+`;
+
+const LOCAL_LOG_OUT = gql`
+  mutation logUserOut {
+    logUserOut @client
+  }
+`;
+
+const ISLOGGEDIN_QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+// ---------------------------------------------------------------------------- //
+
+const Home = props => {
+  const { data: { isLoggedIn } } = useQuery(ISLOGGEDIN_QUERY);
+  const googleSiginMutation = useMutation(GOOGLE_SIGN_IN);
+  const localLoginMutation = useMutation(LOCAL_LOG_IN);
+  const localLogoutMutation = useMutation(LOCAL_LOG_OUT);
+  const [homeText, setHomeText] = useState('Welcome! Please sign in.');
+
+  const responseGoogle = async res => {
+    const { tokenId, googleId } = res;
+
+    if (tokenId && googleId) {
+      const { data: { userLogin: token } } = await googleSiginMutation({
+        variables: {
+          tokenId,
+          googleId
+        }
+      });
+      localLoginMutation({ variables: { token } });
+      setHomeText('Great! You are now logged in.');
+    } else {
+      console.log('Google Sign in error');
+    }
+  };
+
+  const signOutGoogle = () => {
+    localLogoutMutation();
+    setHomeText('Successfully signed out!');
+  };
+
+  return (
+    <Container isSideBar={props.isSideBar}>
+      <HomeContainer>
+        <Title>{homeText}</Title>
+
+        {isLoggedIn ? (
+          <GoogleLogout
+            clientId={googleClientId}
+            buttonText="Sign out"
+            onLogoutSuccess={() => signOutGoogle()}
+          />
+        ) : (
+          <GoogleLogin
+            clientId={googleClientId}
+            buttonText="Sign in with Google"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy="single_host_origin"
+          />
+        )}
+      </HomeContainer>
+    </Container>
+  );
+};
 
 export default withRouteComponent(Home);
