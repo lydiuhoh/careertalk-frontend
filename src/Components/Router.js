6e2d92b75867051d74b0562facd89a53ddf69e3f
junlee91 <jlee757@uic.edu>
2019-05-11 16:45:33 -0500

    Back to original

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-import Auth from '../Routes/Auth';
+// import Auth from '../Routes/Auth';
 import Fairs from '../Routes/Fairs';
 // import About from '../Routes/About';
 import Template from '../Routes/Template';
 // import GetInvolved from '../Routes/GetInvolved';
 
-const PrivateRoutes = () => (
+const Routes = () => (
   <Switch>
     <Route exact path="/" component={Fairs} />
     <Route path="/template" component={Template} />
-  </Switch>
-);
-
-const PublicRoutes = () => (
-  <Switch>
-    <Route exact path="/" component={Auth} />
     <Redirect from="*" to="/" />
   </Switch>
 );
 
-const Router = ({ isLoggedIn }) => {
-  return <BrowserRouter>{isLoggedIn ? <PrivateRoutes /> : <PublicRoutes />}</BrowserRouter>;
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
 };
 
 export default Router;
