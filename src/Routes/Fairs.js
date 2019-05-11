efdc61c46d0e5324d3965f33439c1034b14940d7
junlee91 <jlee757@uic.edu>
2019-05-11 17:06:29 -0500

    Fair page query

--- src/Routes/Fairs.js
+++ src/Routes/Fairs.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useQuery } from 'react-apollo-hooks';
+import { gql } from 'apollo-boost';
 
 import withRouteComponent from './withRouteComponent';
-import { Container } from '../Components/commons';
+import { Container, LoadingLogo } from '../Components/commons';
+import FairCard from '../Components/FairCard';
 
-const Fairs = (props) => (
-  <Container isSideBar={props.isSideBar}>
-    <h1>Fairs</h1>
-  </Container>
+const FAIRS = gql`
+  {
+    getFair {
+      id
+      name
+      address
+      num_of_employers
+      date
+      start_time
+      end_time
+      location
+    }
+  }
+`;
+
+const Wrapper = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Fairs = props => {
+  const {
+    data: { getFair: fairs },
+    loading
+  } = useQuery(FAIRS);
+
+  return (
+    <Wrapper isSideBar={props.isSideBar}>
+      {loading ? <LoadingLogo /> : <FairList fairs={fairs} />}
+    </Wrapper>
+  );
+};
+
+const FairList = ({ fairs }) => (
+  <>
+    {fairs.map((fair, index) => (
+      <FairCard fair={fair} key={index} onClick={() => console.log(fair)} />
+    ))}
+  </>
 );
 
 export default withRouteComponent(Fairs);
