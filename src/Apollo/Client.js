b7e7f65db7ea98f783d793e20e5e47631e39472b
junlee91 <jlee757@uic.edu>
2019-07-13 16:56:00 -0500

    set graphql uri

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -5,7 +5,7 @@ import { defaults, resolvers } from './LocalState';
 export default new ApolloClient({
   uri:
     process.env.NODE_ENV === 'production'
-      ? 'https://careertalk-graphql.herokuapp.com'
+      ? 'https://careertalk-graphql.herokuapp.com/graphql'
       : 'http://localhost:4000/graphql',
   clientState: {
     defaults,
